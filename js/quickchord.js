bb65f99270c0b5e0f292b2ba8461cacca062c0c6
David Kirk <flagbearer223@gmail.com>
2014-02-07 19:23:11 -0500

    No user control, but supports drawing multiple chords across a given canvas

--- js/quickchord.js
+++ js/quickchord.js
@@ -1,9 +1,46 @@
-function main()
+var canvasName = "quickChordCanvas";
+
+var chordArray = 
+[
+	{
+		"name": "B Major",
+		"keys": ["1B", "2Ds", "2Fs"]
+	},
+	{
+		"name": "C Major",
+		"keys": ["1C", "1E", "1G"]
+	},
+	{
+		"name": "C Minor",
+		"keys": ["1C", "1Ds", "1G"]
+	},
+	{
+		"name": "E Major",
+		"keys": ["1E", "1Gs", "2C"]
+	},
+	{
+		"name": "F Minor",
+		"keys": ["1F", "1Gs", "2C"]
+	}
+];
+
+function drawMultipleChords(chordNameArray, width, height)
 {
-	drawChord(0, 0, 1200, 300, ["1A", "1Cs"], "quickChordCanvas");
+	var buffer = 10;
+	var x_pos = 0;
+	var y_pos = 0;
+	var chordWidth = width;
+	var chordHeight = (height/chordNameArray.length)-buffer;
+
+	for(var c = 0; c < chordNameArray.length; c++)
+	{
+		console.log("hello " + y_pos);
+		drawChord(x_pos, y_pos, chordWidth, chordHeight, chordNameArray[c], canvasName);
+		y_pos += chordHeight + buffer;
+	}
 }
 
-function drawChord(x, y, width, height, keys, canvas)
+function drawChord(x, y, width, height, chordName, canvas)
 {
 	var ctx = document.getElementById(canvas).getContext('2d');
 	var keyNameArray = ["1C", "1Cs", "1D", "1Ds", "1E", "1F", "1Fs", "1G", "1Gs", "1A", "1As", "1B", "2C", "2Cs", "2D", "2Ds", "2E", "2F", "2Fs", "2G", "2Gs", "2A", "2As", "2B"];
@@ -12,6 +49,8 @@ function drawChord(x, y, width, height, keys, canvas)
 	var keyHeight = height;
 	var blackHeightRatio = 0.5; //Ratio of black length vs white length
 	var blackWidthRatio = 0.9;
+
+	var keys = getKeysByChord(chordName);
 
 	var nextKeyLocation = 0;
 
@@ -23,7 +62,7 @@ function drawChord(x, y, width, height, keys, canvas)
 		{
 			if($.inArray(keyNameArray[c], keys) != -1) whiteKeyColor = "#AAAAFF";
 
-			drawKey(nextKeyLocation, 0, keyWidth, keyHeight, whiteKeyColor, ctx);
+			drawKey(nextKeyLocation, y, keyWidth, keyHeight, whiteKeyColor, ctx);
 			nextKeyLocation += keyWidth;
 		}
 	}
@@ -39,7 +78,7 @@ function drawChord(x, y, width, height, keys, canvas)
 		{
 			if($.inArray(keyNameArray[c], keys) != -1) blackKeyColor = "#2222FF";
 
-			drawKey(nextKeyLocation-(keyWidth*blackWidthRatio)/2, 0, keyWidth*blackWidthRatio, keyHeight * blackHeightRatio, blackKeyColor, ctx);
+			drawKey(nextKeyLocation-(keyWidth*blackWidthRatio)/2, y, keyWidth*blackWidthRatio, keyHeight * blackHeightRatio, blackKeyColor, ctx);
 		}
 	}
 
@@ -55,5 +94,20 @@ function drawKey(x, y, width, height, color, ctx)
 	ctx.fillRect(x, y, width, height);
 
 	ctx.fillStyle = color;
-	ctx.fillRect(x+outlineThickness, y, width-(outlineThickness*2), height-outlineThickness);
+	ctx.fillRect(x+outlineThickness, y+outlineThickness, width-(outlineThickness*2), height-(outlineThickness*2));
+}
+
+function getKeysByChord(chordName)
+{
+	for(var c = 0; c < chordArray.length; c++)
+	{
+		var workingChord = chordArray[c];
+
+		if(workingChord.name == chordName)
+		{
+			return workingChord.keys;
+		}
+	}
+
+	return [];
 }
